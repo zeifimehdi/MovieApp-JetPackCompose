dae70c8cb3d9a8088529f48438b02808b04e3732
meh <meh@biba.uni-bremen.de>
2023-06-21 16:19:55 +0200

    created recyclerview item

--- app/src/main/java/com/example/movieappusingcompose/MainActivity.kt
+++ app/src/main/java/com/example/movieappusingcompose/MainActivity.kt
@@ -2,31 +2,32 @@ package com.example.movieappusingcompose
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.provider.CalendarContract.Colors
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CardElevation
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -87,7 +88,7 @@ fun MainContent(movieList : List<String> = listOf(
 
             LazyColumn(){
                items(movieList){
-                   itemRow(it)
+                   ItemRow(it)
                }
             }
         }
@@ -96,11 +97,12 @@ fun MainContent(movieList : List<String> = listOf(
 }
 
 @Composable
-fun itemRow(item : String){
+fun ItemRow(item : String){
 
-    Card(modifier = Modifier.padding(5.dp).
-        fillMaxWidth().
-        height(150.dp),
+    Card(modifier = Modifier
+        .padding(5.dp)
+        .fillMaxWidth()
+        .height(150.dp),
         shape = RoundedCornerShape(corner = CornerSize(15.dp)),
         elevation = CardDefaults.cardElevation(
             defaultElevation = 10.dp
@@ -111,6 +113,22 @@ fun itemRow(item : String){
 
     ) {
 
+        Row(modifier = Modifier
+            .padding(5.dp)
+            .fillMaxSize(),
+            Arrangement.Start,
+            Alignment.CenterVertically) {
+            Surface(modifier = Modifier
+                .padding(5.dp)
+                .size(100.dp)
+            ) {
+                Icon(imageVector = Icons.Default.AccountBox, contentDescription = "movie image")
+            }
+
+            Text(text = item , modifier = Modifier.padding(start = 10.dp),
+            style = TextStyle(color = Color.White, fontSize = 12.sp)
+            )
+        }
     }
 }
 
